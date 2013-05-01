d4bf21cc704377dbfe31c4c4dff2733659c8fd31
abartman20 <zarroe13@gmail.com>
2013-04-30 21:21:42 -0500

    added autocomplete functionality

--- js/weather.js
+++ js/weather.js
@@ -3,32 +3,46 @@ $(function() {
   	
 	var updateResultsContainer = function(){
 		var zip = $('#zip').val();
+		var state = "IA";
+		var city = "Iowa City";
 		var apiKey = "c634029e8e35b2db";
-		var url = "http://weather-api.herokuapp.com/weather/" + apiKey + "/conditions/z/" + zip + ".json";
+
+		var url = "http://weather-api.herokuapp.com/weather/" + apiKey + "/conditions/z/" + zip + "/";
 	
-		$.ajax({
-			url: url,
-			success: function(data) {
-				var jData = JSON.parse(data)
-				alert(jData);
-				/*
-				location = jData.location;
-				temp_f = jData.current_observation.temp_f;
-				*/
-			},
-			error: function() {
-				console.log("Error");
-			}
-		})
-	
-		var results = $("div#results")
-		results.html("<h1>Results:</h1>");
-		//results.html("<li>Current Temperature: " + temp_f + "</li>");
-		//results.html("<li>Current Wind Speed: " + wind_mph + "</li>");
+		if(zip != "" && location != "") {
+			$.ajax({
+				url: url,
+				success: function(data) {
+					var jData = JSON.parse(data)
+				
+					var currOb = jData.current_observation;
+					var temp_f = currOb.temperature_string;
+					var wind_mph = currOb.wind_mph;
+					var weather = currOb.weather;
+				
+					var results = $("div#results")
+					results.html("<h1>Results:</h1>");
+					results.append("<li>Current Weather: " + weather + "</li>");
+					results.append("<li>Current Temperature: " + temp_f + "</li>");
+					results.append("<li>Current Wind Speed: " + wind_mph + "</li>");
+				},
+				error: function() {
+					console.log("Error");
+				}
+			})
+		}
 	}
 
 	$('#weather_data').submit(function() {
 		updateResultsContainer();
 		return false;
 	});
-})
+	
+	var input = document.getElementById('place');
+	var options = {
+		types: ['(cities)'],
+		componentRestrictions: {country: 'us'}	
+	};
+
+	autocomplete = new google.maps.places.Autocomplete(input, options);
+});
