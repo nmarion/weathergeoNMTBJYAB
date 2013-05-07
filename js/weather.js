70726dec478eecf287178d6eb91783263d923b5a
abartman20 <zarroe13@gmail.com>
2013-05-07 17:38:44 -0500

    update location results

--- js/weather.js
+++ js/weather.js
@@ -4,31 +4,77 @@ $(function() {
 	var updateResultsContainer = function(){
 		var zip = $('#zip').val();
 		var apiKey = "c634029e8e35b2db";
-		var url = "http://weather-api.herokuapp.com/weather/" + apiKey + "/conditions/z/" + zip + ".json";
-	
-		$.ajax({
-			url: url,
-			success: function(data) {
-				var jData = JSON.parse(data)
-				alert(jData);
-				/*
-				location = jData.location;
-				temp_f = jData.current_observation.temp_f;
-				*/
-			},
-			error: function() {
-				console.log("Error");
-			}
-		})
-	
-		var results = $("div#results")
-		results.html("<h1>Results:</h1>");
-		//results.html("<li>Current Temperature: " + temp_f + "</li>");
-		//results.html("<li>Current Wind Speed: " + wind_mph + "</li>");
+
+		var url = "http://weather-api.herokuapp.com/weather/" + apiKey + "/conditions/z/" + zip + "/";
+
+		
+			$.ajax({
+				url: url,
+				success: function(data) {
+					var jData = JSON.parse(data)
+
+					var currOb = jData.current_observation;
+					var temp_f = currOb.temperature_string;
+					var wind_mph = currOb.wind_mph;
+					var weather = currOb.weather;
+
+					var results = $("div#results")
+					results.html("<h1>Results:</h1>");
+					results.append("<li>Current Weather: " + weather + "</li>");
+					results.append("<li>Current Temperature: " + temp_f + "</li>");
+					results.append("<li>Current Wind Speed: " + wind_mph + "</li>");
+				},
+				error: function() {
+					console.log("Error");
+				}
+			})
+		
 	}
 
-	$('#weather_data').submit(function() {
+		$('#weather_data').submit(function() {
 		updateResultsContainer();
 		return false;
-	});
+		});	
+	
+	
+	var input = document.getElementById('place');
+	var options = {
+		types: ['(cities)'],
+		componentRestrictions: {country: 'us'}	
+	};
+
+	autocomplete = new google.maps.places.Autocomplete(input, options);
+
+	google.maps.event.addListener(autocomplete, 'place_changed', function() {
+		var place = autocomplete.getPlace();
+		
+		var apiKey = "c634029e8e35b2db";
+		var latitude = place.geometry.location.lat();
+		var longitude = place.geometry.location.lng();
+		var url = "http://weather-api.herokuapp.com/weather/" + apiKey + "/conditions/g/" + latitude +"/" + longitude + "/";
+		
+		$.ajax({
+				url: url,
+				success: function(data) {
+					var jData = JSON.parse(data)
+
+					var currOb = jData.current_observation;
+					var temp_f = currOb.temperature_string;
+					var wind_mph = currOb.wind_mph;
+					var weather = currOb.weather;
+
+					var results = $("div#results")
+					results.html("<h1>Results:</h1>");
+					results.append("<li>Current Weather: " + weather + "</li>");
+					results.append("<li>Current Temperature: " + temp_f + "</li>");
+					results.append("<li>Current Wind Speed: " + wind_mph + "</li>");
+				},
+				error: function() {
+					console.log("Error");
+				}
+			
+			})
+	})
+	
+	
 })
