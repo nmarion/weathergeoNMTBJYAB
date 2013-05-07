944600319338755815867a8272b0a594df8a8e3b
Jamie X Yang <jxyang11@gmail.com>
2013-05-07 00:24:44 -0500

    updated js to show location for places w/o submit

--- js/weather.js
+++ js/weather.js
@@ -31,11 +31,12 @@ $(function() {
 		
 	}
 
-	$('#weather_data').submit(function() {
+		$('#weather_data').submit(function() {
 		updateResultsContainer();
 		return false;
-	});
-
+		});	
+	
+	
 	var input = document.getElementById('place');
 	var options = {
 		types: ['(cities)'],
@@ -46,15 +47,34 @@ $(function() {
 
 	google.maps.event.addListener(autocomplete, 'place_changed', function() {
 		var place = autocomplete.getPlace();
+		
 		var apiKey = "c634029e8e35b2db";
 		var latitude = place.geometry.location.lat();
 		var longitude = place.geometry.location.lng();
-		var url = "http://weather-api.herokuapp.com/weather/" + apiKey + "/conditions/q/" + latitude +"," + longitude + "/";
-
-	
-		});
+		var url = "http://weather-api.herokuapp.com/weather/" + apiKey + "/conditions/g/" + latitude +"/" + longitude + "/";
 		
-	
+		$.ajax({
+				url: url,
+				success: function(data) {
+					var jData = JSON.parse(data)
+
+					var currOb = jData.current_observation;
+					var temp_f = currOb.temperature_string;
+					var wind_mph = currOb.wind_mph;
+					var weather = currOb.weather;
+
+					var results = $("div#results")
+					results.html("<h1>Results:</h1>");
+					results.append("<li>Current Weather: " + weather + "</li>");
+					results.append("<li>Current Temperature: " + temp_f + "</li>");
+					results.append("<li>Current Wind Speed: " + wind_mph + "</li>");
+				},
+				error: function() {
+					console.log("Error");
+				}
+			
+			})
+	})
 	
 	
 })
